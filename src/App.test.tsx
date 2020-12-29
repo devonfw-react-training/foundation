478f6b710220bb1249bca10286b20d615aaedf25
Paweł Choniawko <pawel.choniawko@gmail.com>
2020-12-29 22:59:37 +0100

    00-start

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Hello World!/i);
+  expect(titleElement).toBeInTheDocument();
 });
