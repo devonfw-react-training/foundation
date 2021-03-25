95f5673fadee1386b91f948811f21b3856c3e1a8
Paweł Choniawko <pawel.choniawko@gmail.com>
2021-03-25 20:49:19 +0100

    06-forms

--- src/book/components/BookDetails/BookDetails.tsx
+++ src/book/components/BookDetails/BookDetails.tsx
@@ -1,41 +1,53 @@
-import { useState, useEffect, ChangeEvent, SyntheticEvent } from "react";
+import { useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import { useBookService } from "../../services/BooksService";
-import { Book } from "../../book";
+import { BookProperties } from "../../book";
 import { Label } from "./BookDetails.css";
+
+interface ErrorMessages {
+  required: string;
+  maxLength: string;
+  [key: string]: any;
+}
+
+const errorMessages: ErrorMessages = {
+  required: "This field is required",
+  maxLength: "Your input exceed maximum length",
+};
+
+const ErrorMessage = ({ msg }: { msg: string }) => (
+  <div style={{ color: "red" }}>{msg}</div>
+);
 
 interface ParamTypes {
   id: string;
 }
 
-const initBook = { id: NaN, title: "", authors: "" };
+const initBook: BookProperties = { title: "", authors: "" };
 
 export const BookDetails = () => {
   const { save, findOne } = useBookService();
   const { id } = useParams<ParamTypes>();
   const { push } = useHistory();
-  const [book, setBook] = useState<Book>(initBook);
+  const { register, handleSubmit, errors, reset } = useForm({
+    defaultValues: initBook,
+  });
 
   useEffect(() => {
     if (id) {
       findOne(+id).then((book) => {
-        setBook(book);
+        reset(book);
       });
     }
   }, []);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = e.currentTarget;
-    setBook((prevBook) => ({ ...prevBook, [name]: value }));
-  };
-
-  const notifyOnBookChange = (e: SyntheticEvent) => {
-    e.preventDefault();
-    save(book).then(() => push("/book-app/books"));
+  const notifyOnBookChange = (data: BookProperties) => {
+    save({ id: +id, ...data }).then(() => push("/book-app/books"));
   };
   return (
     <div>
-      <form onSubmit={notifyOnBookChange}>
+      <form onSubmit={handleSubmit(notifyOnBookChange)}>
         <div className="form-group row">
           <Label htmlFor="authors" className="col-sm-3 col-form-label">
             Authors:
@@ -46,9 +58,11 @@ export const BookDetails = () => {
               name="authors"
               type="text"
               className="form-control"
-              value={book.authors}
-              onChange={handleChange}
+              ref={register({ required: true })}
             />
+            {errors.authors && (
+              <ErrorMessage msg={errorMessages[errors.authors.type]} />
+            )}
           </div>
         </div>
         <div className="form-group row">
@@ -61,9 +75,11 @@ export const BookDetails = () => {
               name="title"
               type="text"
               className="form-control"
-              value={book.title}
-              onChange={handleChange}
+              ref={register({ required: true, maxLength: 15 })}
             />
+            {errors.title && (
+              <ErrorMessage msg={errorMessages[errors.title.type]} />
+            )}
           </div>
         </div>
         <div className="form-group row">
