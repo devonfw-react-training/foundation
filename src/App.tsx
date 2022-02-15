81aa7c4e28b7c2bdc41323b51ecb03012ef7bae6
Agata Toczyńska <agata.toczynska@capgemini.com>
2022-02-15 15:42:41 +0100

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { BookOverview } from "./book/components/BookOverview/BookOverview";
 import { Container } from "@mui/material";
+import { BookOverview } from "./book/components/BookOverview/BookOverview";
+import { Header } from "./shared/components/Header/Header";
 
 const App = () => (
-  <Container>
-    <BookOverview />
-  </Container>
+  <>
+    <Header />
+    <Container>
+      <BookOverview />
+    </Container>
+  </>
 );
 
 export default App;
