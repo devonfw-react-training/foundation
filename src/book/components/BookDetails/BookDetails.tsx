6a33db082db974b3810c12b9ea969d7d4a291f1f
Agata Toczyńska <agata.toczynska@capgemini.com>
2022-02-17 11:01:17 +0100

    switch to material ui

--- src/book/components/BookDetails/BookDetails.tsx
+++ src/book/components/BookDetails/BookDetails.tsx
@@ -1,33 +1,19 @@
 import { Book } from "../../book";
-import { Label } from "./BookDetails.css";
+import { Card, CardContent, Typography, FormLabel } from "@mui/material";
 
 export interface Props {
   book: Book;
 }
 
 export const BookDetails = (props: Props) => (
-  <div>
-    <form>
-      <div className="form-group row">
-        <Label htmlFor="authors" className="col-sm-3 col-form-label">
-          Authors:
-        </Label>
-        <div className="col-sm-9">
-          <p className="form-control-plaintext" id="authors">
-            {props.book.authors}
-          </p>
-        </div>
-      </div>
-      <div className="form-group row">
-        <Label htmlFor="title" className="col-sm-3 col-form-label">
-          Title:
-        </Label>
-        <div className="col-sm-9">
-          <p className="form-control-plaintext" id="title">
-            {props.book.title}
-          </p>
-        </div>
-      </div>
-    </form>
-  </div>
+  <Card>
+    <CardContent>
+      <FormLabel>Authors:</FormLabel>
+      <Typography>{props.book.authors}</Typography>
+    </CardContent>
+    <CardContent>
+      <FormLabel>Title:</FormLabel>
+      <Typography>{props.book.title}</Typography>
+    </CardContent>
+  </Card>
 );
