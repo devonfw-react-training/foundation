33a233bdc7791bd23c57156a7bf941b3b8678bee
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2022-05-25 20:51:40 +0200

    feat: upgrade tests & forms

--- src/user/components/UserForm/UserForm.tsx
+++ src/user/components/UserForm/UserForm.tsx
@@ -21,7 +21,11 @@ const initUser: UserProperties = { username: "", password: "", email: "" };
 export const UserForm = () => {
   const { saveNew } = useUserService();
   const navigate = useNavigate();
-  const { register, handleSubmit, errors } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: initUser,
   });
   const { mutate } = useMutation<any, Error, UserProperties>(saveNew, {
@@ -33,36 +37,47 @@ export const UserForm = () => {
   const notifyOnUserChange = (data: UserProperties) => {
     mutate(data);
   };
+
+  const { ref: usernameRef, ...usernameProps } = register("username", {
+    required: true,
+  });
+  const { ref: passwordRef, ...passwordProps } = register("password", {
+    required: true,
+  });
+  const { ref: emailRef, ...emailProps } = register("email", {
+    required: true,
+  });
+
   return (
     <form onSubmit={handleSubmit(notifyOnUserChange)}>
       <Stack spacing={4} alignItems="center">
         <TextField
           id="username"
-          name="username"
+          inputRef={usernameRef}
+          {...usernameProps}
           label="Username"
           variant="outlined"
           fullWidth
-          inputRef={register({ required: true })}
           error={!!errors.username}
           helperText={errors.username && errorMessages[errors.username.type]}
         />
         <TextField
           id="password"
-          name="password"
+          inputRef={passwordRef}
+          {...passwordProps}
           label="Password"
           variant="outlined"
           fullWidth
-          inputRef={register({ required: true })}
           error={!!errors.password}
           helperText={errors.password && errorMessages[errors.password.type]}
         />
         <TextField
           id="email"
-          name="email"
+          inputRef={emailRef}
+          {...emailProps}
           label="Email"
           variant="outlined"
           fullWidth
-          inputRef={register({ required: true })}
           error={!!errors.email}
           helperText={errors.email && errorMessages[errors.email.type]}
         />
