fefbf5a20a20411be5ec8bfe69f3b9348f611093
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2022-05-26 20:03:44 +0200

    feat: upgrade tests & forms

--- src/shared/components/Header/Header.test.tsx
+++ src/shared/components/Header/Header.test.tsx
@@ -1,13 +1,12 @@
 import { createRoot } from "react-dom/client";
-
 import { Header } from "./Header";
 
 describe("Header Component", () => {
   it("renders without crashing", () => {
     // given
     const div = document.createElement("div");
+    const root = createRoot(div!);
     // when
-    const root = createRoot(div);
     root.render(<Header />);
     // then no errors thrown
     root.unmount();
