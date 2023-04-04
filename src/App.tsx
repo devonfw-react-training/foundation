20481c8763e5fa285446c2862e50395fce0d6b59
Grzegorz Paprzycki <grzegorz.paprzycki@capgemini.com>
2023-04-04 11:14:17 +0200

    replace class App component with functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 
-interface State {
-  title: string;
-}
+function App() {
+  const [title, setTitle] = useState("Hello World!");
 
-interface Props {}
-
-class App extends Component<Props, State> {
-  state = { title: "Hello World!" };
-
-  render() {
-    return (
-      <Box>
-        <Typography variant="h1" color="primary">
-          {this.state.title}
-        </Typography>
-        <Button onClick={() => this.setState({ title: "New Title" })}>
-          Change title
-        </Button>
-      </Box>
-    );
-  }
+  return (
+    <Box>
+      <Typography variant="h1" color="primary">
+        {title}
+      </Typography>
+      <Button onClick={() => setTitle("New Title")}>Change title</Button>
+    </Box>
+  );
 }
 
 export default App;
