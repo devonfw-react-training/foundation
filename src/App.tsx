e6309749456e3465cb721f8eff0c36e2ca619453
Grzegorz Paprzycki <grzegorz.paprzycki@capgemini.com>
2023-04-06 14:41:21 +0200

    update Router to v6.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import {
-  BrowserRouter,
   Navigate,
-  Route,
-  Routes,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+  useLocation,
 } from "react-router-dom";
 import { BookOverview } from "./book/components/BookOverview/BookOverview";
 import { BookDetails } from "./book/components/BookDetails/BookDetails";
@@ -15,30 +16,55 @@ import { UserProvider } from "./user/services/UserService";
 import { Header } from "./shared/components/Header/Header";
 import { Container } from "@mui/material";
 
-export const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<Navigate to="/book-app/books" replace />} />
-    <Route path="/book-app/books" element={<BookOverview />} />
-    <Route path="/book-app/book" element={<BookDetails />} />
-    <Route path="/book-app/book/:id" element={<BookDetails />} />
-    <Route path="/users/new" element={<UserForm />} />
-    <Route path="/users/list" element={<UserList />} />
-  </Routes>
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/book-app/books",
+        element: <BookOverview />,
+      },
+      {
+        path: "/book-app/book",
+        element: <BookDetails />,
+      },
+      {
+        path: "/book-app/book/:id",
+        element: <BookDetails />,
+      },
+      {
+        path: "/users/new",
+        element: <UserForm />,
+      },
+      {
+        path: "/users/list",
+        element: <UserList />,
+      },
+    ],
+  },
+]);
 
-const App = () => (
-  <BrowserRouter>
-    <QueryClientProvider client={new QueryClient()}>
-      <BookProvider>
-        <UserProvider>
-          <Header />
-          <Container>
-            <AppRoutes />
-          </Container>
-        </UserProvider>
-      </BookProvider>
-    </QueryClientProvider>
-  </BrowserRouter>
-);
+const App = () => <RouterProvider router={router} />;
+
+function Root() {
+  const { pathname } = useLocation();
+
+  return (
+    <>
+      {pathname === "/" ? <Navigate to="/book-app/books" /> : null}
+      <QueryClientProvider client={new QueryClient()}>
+        <BookProvider>
+          <UserProvider>
+            <Header />
+            <Container>
+              <Outlet />
+            </Container>
+          </UserProvider>
+        </BookProvider>
+      </QueryClientProvider>
+    </>
+  );
+}
 
 export default App;
