b991062a87f7293dfccfe275d9dfcc5ed55dbd21
Piotr Kwiakowski <piotr.kwiatkowski@ymail.com>
2023-04-11 21:28:32 +0200

    00-start separate config for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  //@ts-ignore
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js',
-  },
+  plugins: [react()]
 })
